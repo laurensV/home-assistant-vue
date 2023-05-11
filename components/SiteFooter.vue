725d4e87e44e6bd4de45eaa30466b1403f772e7a
Laurens Verspeek <l.verspeek@gmail.com>
2023-05-11 15:28:48 +0200

    add footer

---
+++ components/SiteFooter.vue
@@ -0,0 +1,8 @@
+<template>
+  <footer class="footer has-text-centered">
+    <p class="is-size-7">
+      &copy; {{ new Date().getFullYear() }}
+      <b>Home Assistant Vue</b>
+    </p>
+  </footer>
+</template>
